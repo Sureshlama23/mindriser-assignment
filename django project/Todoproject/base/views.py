d4f27195c4569f789d38039f4346b6007456c17e
suresh <sureshlama812@gmail.com>
2023-12-27 14:37:15 +0545

    todoproject's template folder with index.html file added

--- django project/Todoproject/base/views.py
+++ django project/Todoproject/base/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Todoproject
 
 # Create your views here.
+def home(request):
+    todos = Todoproject.objects.all()
+    print(todos)
+    return render(request,'index.html')
