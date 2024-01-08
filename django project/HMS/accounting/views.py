2ec1ef6b27029e9f98d76ac86ad51b90bd1b455e
suresh <sureshlama812@gmail.com>
2024-01-08 19:58:40 +0545

    inheritance method add

--- django project/HMS/accounting/views.py
+++ django project/HMS/accounting/views.py
@@ -13,6 +13,43 @@ from rest_framework.viewsets import ModelViewSet
 #     return Response(bill_json.data)
 
 class BillView(ModelViewSet):
+    # Method 1 
     queryset = Bill.objects.all()
     serializer_class = BillSerializer
-    
+    # Inheritance method 2
+    def list(self, request):
+        queryset = self.get_queryset()
+        serializer = self.serializer_class(queryset,many=True)
+        return Response(serializer.data)
+        
+    def create(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+    def retrieve(self,request,pk=None):
+        try:
+            queryset = Bill.objects.get(id=pk)
+        except:
+              return Response('Data Not found!')
+        serializer = self.serializer_class(queryset)
+        return Response(serializer.data)
+    def update(self,request,pk=None):
+        try:
+            queryset = Bill.objects.get(id=Pk)
+        except:
+              return Response('Data Not found!')
+        serializer = self.serializer_class(queryset,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+        else:
+            return Response(serializer.errors)
+    def destroy(self,request,pk=None):
+        try:
+            queryset = Bill.objects.get(id=pk)
+        except:
+              return Response('Data Not found!')
+        queryset.delete()
+        return Response('Data delete')
